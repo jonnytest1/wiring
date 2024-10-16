3f448d5bdc0ca5cd881bda6fd62c69fd44481996
jonny <jonathanheindl@gmx.de>
2024-10-16 14:28:17 +0200

    added project

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WiringModule, WiringComponent, templateService } from "wire-lib"
+
+@NgModule({
+  declarations: [
+
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    WiringModule
+  ], exports: [
+    WiringModule
+  ],
+  providers: [{
+    provide: templateService,
+    useValue: () => {
+      return Promise.resolve([])
+    }
+  }],
+  bootstrap: [WiringComponent]
+})
+export class RootMOdule { }
+
+
+
+platformBrowserDynamic().bootstrapModule(RootMOdule, {
+
+})
+  .catch(err => console.error(err));
